9bacb72bc599f319e7f73364ccc7c3fcbc891fb4
Daniel Neves <danbrasileir07@gmail.com>
2023-12-13 20:29:47 -0300

    Templates comentados

--- web-app/src/lessons.ts
+++ web-app/src/lessons.ts
@@ -1660,5 +1660,49 @@ nomes.get(1); // retorna o elemento da segunda posição`,
         }
       ]
     }
+    // },
+    // {
+    //   title: 'TEMPLATES',
+    //   order: 0,
+    //   done: false,
+    //   // Lições
+    //   lessons: [
+    //     // TEMPLATES
+    //     { // bloco de pergunta
+    //       query:
+    //         'pergunta 1 pergunta 1 pergunta 1 pergunta 1 pergunta 1 pergunta 1 pergunta 1 pergunta 1 pergunta 1 ',
+    //       // lista de opções
+    //       options: [
+    //         {
+    //           answer: 'resposta resposta resposta resposta resposta ',
+    //           obs: 'obs obsobsobsobs',
+    //           value: false
+    //         },
+    //         {
+    //           answer: 'resposta resposta resposta resposta resposta ',
+    //           obs: 'obs obsobsobsobs',
+    //           value: true
+    //         },
+    //         {
+    //           answer: 'resposta resposta resposta resposta resposta ',
+    //           obs: 'obs obsobsobsobs',
+    //           value: false
+    //         }
+    //       ],
+    //       selected_index: -1,
+    //       typeItem: 'ItemQuestion',
+    //       correct: false
+    //     },
+    //     { // bloco de código
+    //       content: `const test = "Hello"; console.log(test);`,
+    //       typeItem: 'ItemExampleCode'
+    //     },
+    //     { // bloco de texto
+    //       content:
+    //         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, ex neque corrupti, in quasi corporis libero magni mollitia distinctio repellendus atque deleniti iusto molestias sint cum laboriosam, perferendis fugiat ea.',
+    //       typeItem: 'ItemText'
+    //     }
+    //   ]
+    // }
   ]
 };
